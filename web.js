b98334dce61cf29ff97b550872be84983b886491
delpo <carlosdc360@gmail.com>
2013-07-06 21:29:55 +0000

    prueba con web.js 9

--- web.js
+++ web.js
@@ -5,9 +5,15 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
- var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'));
+ //var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'));
  // response.send(buffer.toString('utf-8');
- response.send('prueba');
+ 
+ fs.readFileSync('index.html', 'utf8', function(err,data){
+   if(err) throw err;
+   response.send(data);
+ });
+
+ //response.send('prueba');
 });
 
 var port = process.env.PORT || 5000;
