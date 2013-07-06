a59a07009898125c69d8da1f6e16686227fb4098
delpo <carlosdc360@gmail.com>
2013-07-06 20:52:35 +0000

    prueba con web.js 6

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(fs.readFileSync('index.html').toString('utf-8'));
-  response.send(buffer);
+  var buffer = new Buffer(fs.readFileSync('index.html'));
+  response.send(buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
