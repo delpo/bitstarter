e4843aa8599173e354fecacb42a085a2219692ca
delpo <carlosdc360@gmail.com>
2013-07-06 21:56:04 +0000

    commit rapido

--- web.js
+++ web.js
@@ -8,12 +8,8 @@ app.get('/', function(request, response) {
  //var buffer = fs.readFileSync('index.html');
  //response.send(buffer.toString('utf8');
  
- fs.readFileSync('index.html', 'utf8', function(err,data){
-  if(err) throw err;
-   var buffer2 = data;
- });
-
-response.send(data.toString());
+ var buffer = fs.readFileSync('index.html', 'utf8');
+ response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
