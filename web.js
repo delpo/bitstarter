f7860fdf1869cc1911f5c3d0c3947ab4939cfbd7
delpo <carlosdc360@gmail.com>
2013-07-06 21:37:53 +0000

    commit rapido

--- web.js
+++ web.js
@@ -5,15 +5,15 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
- //var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'));
- // response.send(buffer.toString('utf-8');
+ var buffer = new Buffer(fs.readFileSync('index.html', 'utf8'));
+ response.send(buffer.toString('utf8');
  
- fs.readFileSync('index.html', 'utf8', function(err,data){
-   if(err) throw err;
-   response.send(data);
- });
+// fs.readFileSync('index.html', 'utf8', function(err,data){
+  // if(err) throw err;
+  // response.send(data);
+// });
 
- response.send('prueba');
+// response.send('prueba');
 });
 
 var port = process.env.PORT || 5000;
